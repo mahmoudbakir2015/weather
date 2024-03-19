7cf815ccad690dfc2fad917a502ffca4be156e37
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-03-19 22:39:49 +0200

    make choose city to be Shared widget

--- lib/presentation/choose_city/choose_city.dart
+++ lib/presentation/choose_city/choose_city.dart
@@ -8,45 +8,75 @@ class ChooseCity extends StatefulWidget {
 }
 
 class _ChooseCityState extends State<ChooseCity> {
+  List<String> country = <String>['One', 'Two', 'Three', 'Four'];
+  String dropdownValue = 'One';
   @override
   Widget build(BuildContext context) {
-    List<String> list = <String>['One', 'Two', 'Three', 'Four'];
-    String dropdownValue = list.first;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: DropdownButton<String>(
-              value: dropdownValue,
-              icon: const Icon(Icons.arrow_downward),
-              elevation: 16,
-              style: const TextStyle(color: Colors.deepPurple),
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurpleAccent,
+      body: Padding(
+        padding: const EdgeInsets.all(
+          20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Choose Your Country',
+              style: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                wordSpacing: 3,
               ),
-              onChanged: (String? value) {
-                // This is called when the user selects an item.
-                setState(() {
-                  dropdownValue = value!;
-                });
-              },
-              items: list.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              'Go to Weather',
+            Center(
+              child: Expanded(
+                child: SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: DropdownButton<String>(
+                    value: dropdownValue,
+                    icon: const Icon(Icons.arrow_drop_down_rounded),
+                    elevation: 16,
+                    alignment: Alignment.center,
+                    isExpanded: true,
+                    style: const TextStyle(color: Colors.deepPurple),
+                    underline: Container(
+                      height: 5,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    onChanged: (String? value) {
+                      // This is called when the user selects an item.
+                      setState(() {
+                        dropdownValue = value!;
+                      });
+                    },
+                    items:
+                        country.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        alignment: Alignment.center,
+                        value: value,
+                        child: Text(
+                          value,
+                          style: const TextStyle(
+                            color: Colors.red,
+                            fontSize: 20,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'Go to Weather',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
